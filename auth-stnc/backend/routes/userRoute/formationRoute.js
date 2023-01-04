3c1a030624153ea810cf931e11465d48077bbf7e
elmehdinainia <mehdinainiaa4@gmail.com>
2023-01-04 14:10:40 +0100

    posser la structure

--- auth-stnc/backend/routes/userRoute/formationRoute.js
+++ auth-stnc/backend/routes/userRoute/formationRoute.js
@@ -1,12 +1,16 @@
 const router = require('express').Router()
 const upload = require('../../outils/implodeImage')
 
-const {CreatFormation,deletformation} = require('../../controllers/usersControllers/formationControllers')
+const {CreatFormation,deletformation,updateformation,getFormation} = require('../../controllers/usersControllers/formationControllers')
 const {tryCatch} =require('../../middlewares/tryCatch')
 const {errorHandler} = require('../../middlewares/errorHandler')
 
 router.post('/createformation', upload.single('images'), tryCatch(CreatFormation))
 router.delete('/deleteFormation/:id', tryCatch(deletformation))
+router.put('/updateFormation/:id', upload.single('images'), tryCatch(updateformation))
+router.get('/getFormation',  tryCatch(getFormation))
+
+
 
 router.use(errorHandler)
 
